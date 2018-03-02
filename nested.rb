8bcf9b0a91417a5b0798ce90756858a8ef40b97a
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 03:53:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,11 +60,15 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
+	# :yukihiro_matsumoto => {
+	#   :known_for => "Ruby",
+	#    :languages => ["LISP, C"]
+	# }
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+   :languages => ["LISP, C"]
+}
+
 
 	programmer_hash =
  		{
@@ -81,7 +85,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+		   :known_for => "Ruby",
+		    :languages => ["LISP, C"]
+		 }
 
 end
 
