dde65e23107823fdc4079397d23ff8d5c09d6693
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 04:09:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,10 +65,10 @@ def adding_matz
 	#    :languages => ["LISP, C"]
 	# }
 
-programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
-   :languages => ["LISP, C"]
-}
+# programmer_hash[:yukihiro_matsumoto] = {
+#   :known_for => "Ruby",
+#    :languages => ["LISP, C"]
+# }
 
 
 	programmer_hash =
@@ -87,7 +87,7 @@ programmer_hash[:yukihiro_matsumoto] = {
         }
      }
 
-		 
+
 		 programmer_hash = {:yukihiro_matsumoto => {
 		   :known_for => "Ruby",
 		    :languages => ["LISP, C"]
